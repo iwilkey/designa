db7851be9c91a21dbe2cb18474a063f7196cef8a
iwilkey <iwilkey@iwu.edu>
2020-07-29 23:50:18 -0500

    pa1.0.7 p3

--- core/src/com/iwilkey/designa/gfx/Camera.java
+++ core/src/com/iwilkey/designa/gfx/Camera.java
@@ -1,21 +1,18 @@
 package com.iwilkey.designa.gfx;
 
 import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 
-import com.iwilkey.designa.Game;
 import com.iwilkey.designa.GameBuffer;
 import com.iwilkey.designa.entities.Entity;
+import com.iwilkey.designa.tiles.Tile;
 
 public class Camera {
 
     private GameBuffer gb;
 
     private Vector3 position;
-    private Vector2 scale;
-
-    private Vector2 dPos;
+    private Vector3 offset;
 
     public static Matrix4 mat = new Matrix4();
 
@@ -23,35 +20,43 @@ public class Camera {
 
     public Camera(GameBuffer gb, int x, int y) {
         this.gb = gb;
-        position = new Vector3(x, y, 0);
-        scale = new Vector2(1,1);
-
-        dPos = new Vector2(0, 0);
+        position = new Vector3(0,0,0);
+        offset = new Vector3(0,0,0);
     }
 
-    private void checkWhiteSpace() {
-        if(position.x < 0) {
-            position.x = 0;
+    private void translate() {
+        if(offset.x < 0 && offset.y < 0) {
+            mat.setTranslation(new Vector3(0, 0, 0));
+        } else if (offset.x < 0) {
+            mat.setTranslation(new Vector3(0, -offset.y, 0));
+        } else if (offset.y < 0) {
+            mat.setTranslation(new Vector3(-offset.x, 0, 0));
+        } else if (offset.x > gb.getWorld().w * Tile.TILE_SIZE - gb.getGame().w &&
+                offset.y > gb.getWorld().h * Tile.TILE_SIZE + gb.getGame().h) {
+            mat.setTranslation(new Vector3(-(gb.getWorld().w * Tile.TILE_SIZE - gb.getGame().w),
+                    -(gb.getWorld().h * Tile.TILE_SIZE - gb.getGame().h), 0));
+        } else if (offset.x > gb.getWorld().w * Tile.TILE_SIZE - gb.getGame().w) {
+            mat.setTranslation(new Vector3(-(gb.getWorld().w * Tile.TILE_SIZE - gb.getGame().w),
+                    -offset.y, 0));
+        } else if (offset.y > gb.getWorld().h * Tile.TILE_SIZE + gb.getGame().h) {
+            mat.setTranslation(new Vector3(-offset.x,
+                    -(gb.getWorld().h * Tile.TILE_SIZE - gb.getGame().h), 0));
+        } else {
+            mat.setTranslation(new Vector3(-offset.x, -offset.y, 0));
         }
     }
 
     public void centerOnEntity(Entity e) {
+        float targxOffset = e.getX() - (gb.getGame().w / 2) + e.getWidth() / 2;
+        float targyOffset = e.getY()- (gb.getGame().h / 2) + e.getHeight() / 2;
 
-        float targxOffset = (Math.abs(Game.w - e.getX() - 32) - (gb.getGame().w / 2) + e.getWidth() / 2);
-        float targyOffset = (Math.abs(Game.h - e.getY()) - (gb.getGame().h / 2) + e.getHeight() / 2) - 16;
-
-        dPos.x = (((int)targxOffset - position.x) * camSpeed * 0.01f);
-        dPos.y = (((int)targyOffset - position.y) * camSpeed * 0.01f);
-
-        checkWhiteSpace();
-
+        offset.x += (((int)targxOffset - offset.x) * camSpeed * 0.01f);
+        offset.y += (((int)targyOffset - offset.y) * camSpeed * 0.01f);
     }
 
     public void tick() {
         mat.getTranslation(position);
-        mat.translate(new Vector3(dPos.x, dPos.y, 0));
-        mat.scale(scale.x, scale.y, 1);
+        translate();
     }
-
 
 }
